786b3b4157c688279e7f694a50fc34a92764de67
Franz Mejia <mejiafranz@gmail.com>
2022-07-13 17:34:37 -0400

    Init project degree monitoring

---
+++ src/main/java/com/packt/sysprojectdegree/SysprojectdegreeApplication.java
@@ -0,0 +1,13 @@
+package com.packt.sysprojectdegree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysprojectdegreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysprojectdegreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/packt/sysprojectdegree/SysprojectdegreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packt.sysprojectdegree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysprojectdegreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
