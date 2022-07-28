38bc819bdce40986827059506f53eccb9e72812d
Julio Cesar Cori Ochoa <ingeumsacori@gmail.com>
2022-07-27 21:18:24 -0400

    feat: Add cronograma model, related 'ManyToOne' with the project

---
+++ src/main/java/com/packt/sysprojectdegree/cronograma/Cronograma.java
@@ -0,0 +1,181 @@
+package com.packt.sysprojectdegree.cronograma;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.packt.sysprojectdegree.project.Project;
+
+import org.springframework.data.annotation.CreatedDate;
+
+@Entity
+public class Cronograma {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String type;
+    private Integer duration;
+    @CreatedDate
+    private Instant date;
+
+    @ManyToOne()
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    public Cronograma() {
+
+    }
+
+    /**
+     * @param type
+     * @param duration
+     * @param date
+     * @param project
+     */
+    public Cronograma(String type, Integer duration, Instant date, Project project) {
+        this.type = type;
+        this.duration = duration;
+        this.date = date;
+        this.project = project;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the duration
+     */
+    public Integer getDuration() {
+        return duration;
+    }
+
+    /**
+     * @param duration the duration to set
+     */
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    /**
+     * @return the date
+     */
+    public Instant getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the project
+     */
+    public Project getProject() {
+        return project;
+    }
+
+    /**
+     * @param project the project to set
+     */
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((date == null) ? 0 : date.hashCode());
+        result = prime * result + ((duration == null) ? 0 : duration.hashCode());
+        result = prime * result + ((project == null) ? 0 : project.hashCode());
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Cronograma))
+            return false;
+        Cronograma other = (Cronograma) obj;
+        if (date == null) {
+            if (other.date != null)
+                return false;
+        } else if (!date.equals(other.date))
+            return false;
+        if (duration == null) {
+            if (other.duration != null)
+                return false;
+        } else if (!duration.equals(other.duration))
+            return false;
+        if (project == null) {
+            if (other.project != null)
+                return false;
+        } else if (!project.equals(other.project))
+            return false;
+        if (type == null) {
+            if (other.type != null)
+                return false;
+        } else if (!type.equals(other.type))
+            return false;
+        return true;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    
+    @Override
+    public String toString() {
+        return "Cronograma [date=" + date + ", duration=" + duration + ", id=" + id + ", project=" + project + ", type="
+                + type + "]";
+    }
+
+
+
+
+}
