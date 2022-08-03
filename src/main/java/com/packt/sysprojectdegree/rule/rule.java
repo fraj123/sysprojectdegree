419d9b34a49ae4555da831f621eef9153048abcc
aledcq <aledcq@gmail.com>
2022-08-03 12:55:39 -0400

    prueba push usando SSH Key

--- src/main/java/com/packt/sysprojectdegree/rule/rule.java
+++ src/main/java/com/packt/sysprojectdegree/rule/rule.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+//ENTIDAD
 @Entity
 public class rule {
 
